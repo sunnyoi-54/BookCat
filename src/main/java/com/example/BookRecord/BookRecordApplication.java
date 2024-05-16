546a847679d67b4af075e1893bd9dd101a6b88de
sunyoung <minnie2002@naver.com>
2024-05-16 15:24:56 +0900

    first

---
+++ src/main/java/com/example/BookRecord/BookRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookRecord/BookRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
